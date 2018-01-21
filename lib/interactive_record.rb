f809201ca494fd8bcc5f091ce498b2c440d23750
Brad Hambrice <hambrice@uab.edu>
2018-01-21 00:36:28 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -17,6 +17,10 @@ class InteractiveRecord
     column_names.compact
   end
 
+  self.column_names.each do |col_name|
+      attr_accessor col_name.to_sym
+    end
+
   def initialize(options = {})
     options.each do |key, value| self.send("#{key}=", value)
     end
